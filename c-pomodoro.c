b9ced5d4d2e3e46eea00f7f1b87e79b8d84dacd7
Archie Collison <archie.collison@gmail.com>
2023-05-17 20:25:17 +1000

    Rest timer functionality, runs indefinitely

--- c-pomodoro.c
+++ c-pomodoro.c
@@ -52,21 +52,16 @@ void update_time(int *hour, int *min, int *sec) {
     *min = info->tm_min;
     *sec = info->tm_sec;
 
-    printf("\t\t\t");
-    text_bold(stdout);
-    background_red(stdout);
-    printf("%02d:%02d:%02d", *hour, *min, *sec);
-    reset_colors(stdout);
-    printf("\n");
-
+    
 }
 void refresh_line() {
-    printf("\x1b[2K");
+    //printf("\x1b[2K"); // works on ANSI terminals, demands POSIX
     printf("\x1b[1F"); // Move to beginning of previous line
     printf("\x1b[2K"); // Clear entire line [ANSI escape codes]
 }
 
 void success_time(int hour, int min, int sec, int mins) {
+    update_time(&hour, &min, &sec);
     min += mins;
     if (mins > 59) {
         mins = mins % 60;
@@ -74,8 +69,8 @@ void success_time(int hour, int min, int sec, int mins) {
         to_12hr(hour);
     }
 
-    text_bold(stdout);
     printf("\t\t\t");
+    text_bold(stdout);
     background_cyan(stdout);
     printf("%02d:%02d:%02d", hour, min, sec);
     reset_colors(stdout);
@@ -83,62 +78,68 @@ void success_time(int hour, int min, int sec, int mins) {
 }
 
 void pd_success() {
-    printf("\n");
     printf("\t\t\t");
     text_bold(stdout);
     text_green(stdout);
     printf("success!");
     reset_colors(stdout);
 
-    printf("\n\n\n");
+    printf("\n\n");
 }
 
-void command_prompt(char length_str[MAX_CMD_LEN], char rest_length_str[MAX_CMD_LEN], int *length, int *rest_length) {
+void command_prompt(int *length, int *rest_length) {
     printf("pomodoro length (1-60): ");
-    fgets(length_str, MAX_CMD_LEN, stdin);
+    scanf(" %d", length);
     printf("rest length (1-60): ");
-    fgets(rest_length_str, MAX_CMD_LEN, stdin);
-
-    *length = atoi(length_str);
-    *rest_length = atoi(rest_length_str);
+    scanf(" %d", rest_length);
 }
 
-void pomodoro_display(int *hour,int *min,int *sec,int length) {
+void pomodoro_display(int *hour,int *min,int *sec, int length) {
     int i = 0;
+
     while (i < length * 60) {
         update_time(hour, min, sec);
+        printf("\t\t\t");
+        text_bold(stdout);
+        background_red(stdout);
+        printf("%02d:%02d:%02d", *hour, *min, *sec);
+        reset_colors(stdout);
+        printf("\n");
         sleep(1);
         refresh_line();
         i++;
     }
 }
-int continue_prompt() {
+
+int continue_prompt(int *length, int *rest_length) {
     text_underline(stdout);
     printf("begin rest timer?");
     reset_colors(stdout);
-    printf(" (y/n): ");
+    printf(" (y/n/c): ");
     char input;
-    fgets(&input, 2, stdin);
+    scanf(" %c", &input);
 
     if (input == 'y') {
         return 1;
     } else if (input == 'n') {
         return 0;
+    } else if (input == 'c') {
+        command_prompt(length, rest_length);
+        return 1;
     } else {
-        continue_prompt();
+        continue_prompt(length, rest_length);
     }
+    return 0;
 }
 
 int main(void) {
-    char length_str[MAX_CMD_LEN];
-    char rest_length_str[MAX_CMD_LEN];
+    char length_str;
+    char rest_length_str;
     int length = 0;
     int rest_length = 0;
-    printf("\e[1;1H\e[2J"); // works on ANSI terminals, demands POSIX
-    command_prompt(length_str, rest_length_str, &length, &rest_length);
+    printf("\e[1;1H\e[2J"); // clears terminal. works on ANSI terminals, demands POSIX
+    command_prompt(&length, &rest_length);
 
-    printf("\e[1;1H\e[2J"); // works on ANSI terminals, demands POSIX
-    printf("\n\n\n");
 
     // get time values
     time_t rawtime;
@@ -149,15 +150,27 @@ int main(void) {
     int min = info->tm_min;
     int sec = info->tm_sec;
     // pomodoro times
+
     success_time(hour, min, sec, length);
     pomodoro_display(&hour, &min, &sec, length);
     pd_success();
-    if (continue_prompt()) {
-        update_time(&hour, &min, &sec);
-        printf("\e[1;1H\e[2J"); // works on ANSI terminals, demands POSIX
-        printf("\n\n\n");
+    int looping = continue_prompt(&length, &rest_length);
+
+    while (looping) {
+        // rest timer, bad pattern
+        // TODO: fix
+        printf("\e[1;1H\e[2J");
         success_time(hour, min, sec, rest_length);
         pomodoro_display(&hour, &min, &sec, rest_length);
+        // repeat ?
+        looping = continue_prompt(&length, &rest_length);
+        // work timer
+        printf("\e[1;1H\e[2J");
+        success_time(hour, min, sec, length);
+        pomodoro_display(&hour, &min, &sec, length);
+        pd_success();
+        // repeat ?
+        looping = continue_prompt(&length, &rest_length);
     }
     return 0;
 }
